94c52d6343555174e3c4cd8caaa5f5629a3ff8fc
Vivek Jauhari <64536943+vivekjauhari@users.noreply.github.com>
2023-09-04 15:03:07 +1000

    Added module

--- main.tf
+++ main.tf
@@ -23,11 +23,24 @@ resource "aws_instance" "blogs" {
   instance_type = var.instance_type
 
 
-  vpc_security_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  vpc_id = data.aws_vpc.default.id
+  name = "blog_new"
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp]
+  ingress-cidr_blocks = ["0.0.0.0/0"]
+
+  egress_fules = ["all-all"]
+  egress-cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog"{

25e096b107886fa47079e572e8addd46b1f58618
Vivek Jauhari <64536943+vivekjauhari@users.noreply.github.com>
2023-09-04 16:14:35 +1000

    modifed from default

--- main.tf
+++ main.tf
@@ -20,10 +20,8 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
@@ -31,16 +29,20 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blogs" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "blog"
 
-  tags = {
-    Name = "HelloWorld"
-  }
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
@@ -77,18 +79,14 @@ module "blog_alb" {
   }
 }
 
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  vpc_id = module.blog_vpc.vpc_id
-  name = "blog"
+  version = "4.13.0"
 
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-

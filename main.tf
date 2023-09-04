ea7b7273f6752ee879a20c7d6907af1b56383001
Vivek Jauhari <64536943+vivekjauhari@users.noreply.github.com>
2023-09-04 15:05:03 +1000

    updated

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_sg" {
   vpc_id = data.aws_vpc.default.id
   name = "blog_new"
 
-  ingress_rules = ["http-80-tcp", "https-443-tcp]
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress-cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules = ["all-all"]

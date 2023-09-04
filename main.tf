7a217e63ba1a1662e9b6f1765ec7cf37000d1a94
Vivek Jauhari <64536943+vivekjauhari@users.noreply.github.com>
2023-09-04 15:59:08 +1000

    refixed

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = "aws_instance.blog.id"
+          target_id = aws_instance.blog.id
           port = 80
         }
       }

5658d12b48754140cd85c048bd651060c39fd7c8
Vivek Jauhari <64536943+vivekjauhari@users.noreply.github.com>
2023-09-04 16:16:07 +1000

    updated output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "instance_ami" {
-  value = aws_instance.blogs.ami
+  value = aws_instance.blog.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blogs.arn
+  value = aws_instance.blog.arn
 }

27f0c22bef578326371367457e16176e12e47637
Vivek Jauhari <64536943+vivekjauhari@users.noreply.github.com>
2023-09-04 15:04:11 +1000

    updated

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "blog_sg" {
   ingress_rules = ["http-80-tcp", "https-443-tcp]
   ingress-cidr_blocks = ["0.0.0.0/0"]
 
-  egress_fules = ["all-all"]
+  egress_rules = ["all-all"]
   egress-cidr_blocks = ["0.0.0.0/0"]
 }
 
